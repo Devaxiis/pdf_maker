d872bb09f68fe58661468fc800e86664eb7c0b58
Devaxiis <mirjalolabdunazarov1425@gmail.com>
2025-06-25 11:28:26 +0500

    github ahifasi qo'shildi

---
+++ lib/home/github/github.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GithubPage extends StatefulWidget {
+  const GithubPage({super.key});
+
+  @override
+  State<GithubPage> createState() => _GithubPageState();
+}
+
+class _GithubPageState extends State<GithubPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
